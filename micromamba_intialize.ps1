cd0eae490e61663ee1ccea541df99906e69fd0ad
Danish bin Abdul Fata <124814346+danish-abdulfata@users.noreply.github.com>
2024-10-28 15:20:00 +0000

    Create micromamba_intialize.ps1

---
+++ micromamba_intialize.ps1
@@ -0,0 +1,4 @@
+micromamba.exe shell hook -s powershell | Out-String | Invoke-Expression
+micromamba activate
+micromamba activate supy_lcz
+cd C:\Users\Danish\Documents\GitHub\UrbClimUGdissert\supy-lcz-global
