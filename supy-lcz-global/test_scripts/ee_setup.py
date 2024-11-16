41a5ab0e25d3e75bb9f16607e19d3cecba3d04d9
Danish bin Abdul Fata <124814346+danish-abdulfata@users.noreply.github.com>
2024-11-16 20:26:44 +0000

    wsl2

--- supy-lcz-global/test_scripts/ee_setup.py
+++ supy-lcz-global/test_scripts/ee_setup.py
@@ -1,5 +1,5 @@
 import ee
 
-ee.Authenticate()
+ee.Authenticate(force=True, auth_mode=gcloud)
 ee.Initialize(project='uclgeodissertation')
 print(ee.String('Hello from the Earth Engine servers!').getInfo())
