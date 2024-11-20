06eeb3714149f63c5da2b870968e42a5bcafcec6
Danish bin Abdul Fata <124814346+danish-abdulfata@users.noreply.github.com>
2024-11-20 00:27:02 +0000

    Update run_split_models.py

--- supy-lcz-global/test_scripts/run_split_models.py
+++ supy-lcz-global/test_scripts/run_split_models.py
@@ -9,17 +9,31 @@ from pyproj import Transformer
 
 # Model Parameters
 
-# Site area
+# Site Information
+sitename = KL-KualaLumpur
 site_midpoint_lat = 3.056577
 site_midpoint_lon = 101.617373
+
+measurement_height_above_ground = 100
+surface_cover_radius = 1000 
+time_analysis_start = "2016-01-01 00:00:00"
+time_coverage_end = "2017-01-01 00:00:00"
+timestep_interval_seconds = 3600
+local_utc_offset_hours = 8
+
+# spinup = true
+# need to check further;
+# modify runner script so that we can define spinup here?
 
 # Total area covered will be grid_size * grid_boxes, in m^2
 grid_size = 1000 
 grid_boxes = 40
 
-# By what factor should the area be divided, 1 = 1/4, 2 = 1/4^2, 3 = 1/4^3 etc, MAINTAINING SQUARE STUDY AREA.   
-factor = 2
+# By what factor should the area be divided, 1 = 1/4, 2 = 1/4^2, 3 = 1/4^3 and etc, to maintain square study area for compatibility with utils.py.   
+split_factor = 2
 
+
+# modified from utils.py to maintain consistency with geodesic calculations
 crs_dict = {
             'proj': 'utm',
             'zone': int(np.round((183 + site_midpoint_lon) / 6)),
@@ -37,19 +51,20 @@ site_y_min = site_y_max - ((grid_boxes - 1) * grid_size)
 site_x_max = site_midpoint_x + (distance_from_midpoint)
 site_x_min = site_x_max - ((grid_boxes - 1) * grid_size)
 
-site_midpoint_y = np.linspace(site_y_min, site_y_max, 4**factor)
-site_midpoint_x = np.linspace(site_x_min, site_x_max, 4**factor)
+site_midpoint_y = np.linspace(site_y_min, site_y_max, 4**split_factor, endpoint = false)
+site_midpoint_x = np.linspace(site_x_min, site_x_max, 4**split_factor, endpoint = false)
 
-xx, yy = np.meshgrid(site_midpoint_y, site_midpoint_x)
+split_xx, split_yy = np.meshgrid(site_midpoint_y, site_midpoint_x)
 
 print("Site min and max points")
 print(site_midpoint_y)
 print(site_midpoint_x)
 
 print("Site midpoints")
-print(xx)
-print(yy)
+print(split_xx)
+print(split_yy)
 
+# copied from utils.py
 # def from_point(
 #            cls,
 #            *,
