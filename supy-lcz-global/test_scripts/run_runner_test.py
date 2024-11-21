8966557428c4adeef94bda1f2e0786c8ddb7f49e
Danish bin Abdul Fata <124814346+danish-abdulfata@users.noreply.github.com>
2024-11-21 21:59:53 +0000

    test

--- supy-lcz-global/test_scripts/run_runner_test.py
+++ supy-lcz-global/test_scripts/run_runner_test.py
@@ -1,31 +1,26 @@
-import os
-os.environ['USE_PYGEOS'] = '0'
-
 import time
-import argparse
 from pathlib import Path
-from runner.runner import main as run_supy
+from runner.runner import main as run_runner
 
 ############################# Testing for run_split_models.py, 2nd part.
 
 split_runs_start = time.time()
-
 site_list = ['KL-KualaLumpurTest1', 'KL-KualaLumpurTest2', 'KL-KualaLumpurTest3']
 
 number_of_runs = 3
-
+split_grid_length = 5
 split_run_count = 0
 for individual_split_site in site_list:
-    run_supy([individual_split_site,
+    run_runner([individual_split_site,
         '--run-type', 'grid',
         '--grid-size', '1000',
-        '--grid-boxes', '20',
+        '--grid-boxes', str(split_grid_length),
         '--metforc-src', 'era5land',
         '--urbdesc-src', 'lcz_updated',
         '--sitelist', 'sitelist_custom',
         '--download-era5'])
     split_run_count += 1
-    print(f"====> Split Run #{split_run_count} completed out of {number_of_runs} for {individual_split_run}") 
+    print(f"====> Split Run #{split_run_count} completed out of {number_of_runs} for {individual_split_site}") 
     print(f"====> Split Run {individual_split_site} completed out of {number_of_runs} for site_name_prefix") 
         
 split_runs_end = time.time()
--- supy-lcz-global/test_scripts/run_split_models.py
+++ supy-lcz-global/test_scripts/run_split_models.py
@@ -1,6 +1,3 @@
-import os
-os.environ['USE_PYGEOS'] = '0'
-
 import time
 from pathlib import Path
 import pandas as pd
@@ -29,8 +26,11 @@ local_utc_offset_hours = 8
 # Change default spinup (2 years) in runner.py at line 868
 
 # Total area covered will be grid_size^2 * grid_boxes, in m^2
-grid_size = 1000 
-grid_boxes = 40
+# grid_size
+grid_metre_length = 1000 
+
+# grid_boxes
+site_grid_length = 40
 
 # By what factor should the area be divided, ie. 2^2(split_factor) models will sequentially run.   
 # In other words, split models will run with 1 = 1/4, 2 = 1/4^2, 3 = 1/4^3, etc, number of grids.
@@ -45,17 +45,20 @@ run_split_models_start = time.time()
 
 # Split factor check if it's valid
 #'2^2(split_factor)' simplifies to '4 ** split_factor' for readability.
-number_of_runs = 4 ** split_factor
+site_grid_area = site_grid_length ** 2
+site_split_length = 2 ** split_factor
+number_of_runs = site_split_length ** 2
+split_grid_area = site_grid_area / number_of_runs
+split_grid_length = int(site_grid_length / site_split_length)
 
 try:
-    (grid_boxes ** 2) % number_of_runs == 0
+    (site_grid_length) % split_grid_length == 0
 except:
-    raise ValueError(f"Total number of grids ({grid_boxes ** 2}) must be divisible by 2^2(split_factor) ({number_of_runs}) to maintain identical square model areas")
+    raise ValueError(f"Total number of grids ({site_grid_area}) must be divisible by 2^2(split_factor) ({number_of_runs}) to maintain identical square model areas")
 else:
-    split_grid_boxes = grid_boxes / (2 ** split_factor)
-    print(f"Split factor of {split_factor} is valid. Site will be split into {number_of_runs} runs")
-             
+    print(f"Split factor of {split_factor} is valid. Site will be split into {number_of_runs} runs at {split_grid_area} grids per run")
 
+#raise SystemExit()
 # Geodesic calculations modified from utils.py to maintain consistency
 
 crs_dict = {
@@ -71,16 +74,16 @@ to_utm = Transformer.from_crs(crs_from='EPSG:4326', crs_to=crs)
 site_midpoint_x, site_midpoint_y = to_utm.transform(xx=site_midpoint_lat, yy=site_midpoint_lon)
 
 # identify site boundaries
-site_area_length = grid_size * grid_boxes
+site_metre_length = grid_metre_length  * site_grid_length
 
-site_y_max = site_midpoint_y + (site_area_length / 2)
-site_y_min = site_y_max - (site_area_length)
-site_x_max = site_midpoint_x + (site_area_length / 2)
-site_x_min = site_x_max - (site_area_length)
+site_y_max = site_midpoint_y + (site_metre_length / 2)
+site_y_min = site_y_max - (site_metre_length)
+site_x_max = site_midpoint_x + (site_metre_length / 2)
+site_x_min = site_x_max - (site_metre_length)
 
 # +1 to account for the additional sample at the start, [1:] to remove before meshing.
-split_midpoint_x = np.linspace(site_x_min, site_x_max, (2**split_factor) + 1, endpoint = False)[1:]
-split_midpoint_y = np.linspace(site_y_min, site_y_max, (2**split_factor) + 1, endpoint = False)[1:]
+split_midpoint_x = np.linspace(site_x_min, site_x_max, site_split_length + 1, endpoint = False)[1:]
+split_midpoint_y = np.linspace(site_y_min, site_y_max, site_split_length + 1, endpoint = False)[1:]
 
 # converting back to latlong
 from_utm = Transformer.from_crs(crs_from=crs, crs_to='EPSG:4326')
@@ -110,12 +113,12 @@ for split_affix in range(1, number_of_runs + 1):
     split_site_list.append(site_prefix + "_s" + str(split_affix))
  
 # creates dataframe with index column only
-df = pd.DataFrame(index=split_site_list)
-df.index.name = 'sitename'
+split_site_list_df = pd.DataFrame(index=split_site_list)
+split_site_list_df.index.name = 'sitename'
 
 # add specified latlong column headers and data to df
-df.insert(0, 'latitude', lat_list)
-df.insert(1, 'longitude', lon_list)
+split_site_list_df.insert(0, 'latitude', lat_list)
+split_site_list_df.insert(1, 'longitude', lon_list)
 
 column_dict = {'measurement_height_above_ground': measurement_height_above_ground,
         'surface_cover_radius': surface_cover_radius,
@@ -126,13 +129,13 @@ column_dict = {'measurement_height_above_ground': measurement_height_above_groun
 
 # for loop to add parameters and their columns into data frame df . reversed() used because columns were placed from end to start.
 column_index = 2
-for column_key, column_value in reversed(column_dict.items()):
-    df.insert(column_index, column_key, column_value)
+for column_key, column_value in column_dict.items():
+    split_site_list_df.insert(column_index, column_key, column_value)
     column_index += 1
     
 # create .csv file at specified filepath
-split_site_list_file = Path(f'resources/{site_prefix}_custom.csv')
-df.to_csv(split_site_list_file)
+split_site_list_file = Path(f'resources/{site_prefix}_splitlist_custom.csv')
+split_site_list_df.to_csv(split_site_list_file)
 
 try:
     split_site_list_file.exists()
@@ -142,6 +145,7 @@ except:
 else:
     print(f"--------> {site_prefix}_custom.csv successfully created. Starting SuPy models...")
     
+# raise SystemExit()
 ################################ 2nd part of script ################################
 ########### Run SuPy runner.runner script for all split sites using processed inputs.
 
@@ -151,11 +155,11 @@ split_run_count = 0
 for individual_split_site in split_site_list:
     run_runner([individual_split_site,
             '--run-type', 'grid',
-            '--grid-size', str(grid_size),
-            '--grid-boxes', str(split_grid_boxes),
+            '--grid-size', str(grid_metre_length),
+            '--grid-boxes', str(split_grid_length),
             '--metforc-src', 'era5land', # change if needed
             '--urbdesc-src', 'lcz_updated', #  change if needed
-            '--sitelist', f'{site_prefix}_custom',
+            '--sitelist', f'{site_prefix}_splitlist_custom',
             '--download-era5',
             '--do-spinup']) # remove --do-spinup to disable spinup
     split_run_count += 1
