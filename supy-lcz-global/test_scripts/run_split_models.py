234e63e5a72f4efc4420d2ab02b8d3a66178e73c
Danish bin Abdul Fata <124814346+danish-abdulfata@users.noreply.github.com>
2024-11-26 20:39:10 +0000

    Update run_split_models.py

--- supy-lcz-global/test_scripts/run_split_models.py
+++ supy-lcz-global/test_scripts/run_split_models.py
@@ -185,9 +185,9 @@ for individual_split_site in split_site_list_df.index:
     individual_split_lat = split_site_list_df.iloc[individual_split_site, 1]
     individual_split_lon = split_site_list_df.iloc[indvidual_split_site, 2]
     
-    individual_split_path = Path(f'data/{individual_split_name}/output/grid/df_output_uMF_uLCu.h5')
+    individual_split_path = f'data/{individual_split_name}/output/grid'
     
-    grid_out.convert_h5_to_netcdf(individual_split_path, 1000, individual_split_lat, individual_split_lon)
+    grid_out.convert_h5_to_netcdf(individual_split_path + '/df_output_uMF_uLCu.h5'), 1000, individual_split_lat, individual_split_lon)
     split_run_count += 1
     
     
