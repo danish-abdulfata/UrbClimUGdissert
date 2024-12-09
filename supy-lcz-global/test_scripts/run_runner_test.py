c8f5872a07f50110cb25059b410e1645cf7cb533
danish-abdulfata <124814346+danish-abdulfata@users.noreply.github.com>
2024-12-09 18:26:00 +0000

    finishing up on dataframe merge

--- supy-lcz-global/test_scripts/run_runner_test.py
+++ supy-lcz-global/test_scripts/run_runner_test.py
@@ -1,14 +1,16 @@
 import time
 import pandas as pd
+import numpy as np
 from pathlib import Path
 from pyproj import CRS
 from pyproj import Transformer
+import xarray as xr
 from runner.runner import main as run_runner
 ############################# Testing for run_split_models.py, 2nd part.
 
 split_runs_start = time.time()
-site_list_df = pd.read_csv(Path('resources/KL-KualaLumpur-2017_1Msp_1_splitlist.csv'))
-site_list_df.index.name = 'sitename'
+split_site_list_df = pd.read_csv(Path('resources/KL-KualaLumpur-2017_1Msp_1_splitlist.csv'))
+split_site_list_df.index.name = 'sitename'
 number_of_runs = 3
 split_grid_length = 5
 split_grid_area = split_grid_length**2
@@ -47,15 +49,11 @@ site_prefix = "KL-KualaLumpur-2016"
 #  merging into one, output file with grid coordinates
 
 # relabelling grid indexes testing
-split_file_count = 1
-model_run = 'KL-KualaLumpur-2017_1Msp_1_s1'
-test_out_df = pd.read_hdf(f'data/{model_run}/output/grid/df_output_uMF_uLCu.h5')
-print(test_out_df)
-# file_grid_number = test_out_df.index.levels[0]
-# modified_grid_numbers = file_grid_number + (split_file_count * split_grid_area)
-# grid_number_dict = dict(list(zip(file_grid_number.to_list(), modified_grid_numbers.to_list())))
-# print(grid_number_dict)
-# test_out_df = test_out_df.rename(grid_number_dict, axis = 0, level = 'grid')
+
+# model_run = 'KL-KualaLumpur-2017_1Msp_1_s1'
+# test_out_df = pd.read_hdf(f'data/{model_run}/output/grid/df_output_uMF_uLCu.h5')
+# print(test_out_df)
+
 # print(test_out_df)
 
 # defining list for all variables
@@ -64,55 +62,96 @@ print(test_out_df)
 
 variable_list = ['Kdown', 'Kup', 'Ldown', 'Lup', 'Tsurf', 'QN', 'QF', 'QS', 'QH', 'QE', 'QHlumps', 'QElumps', 'QHresis', 'Rain', 'Irr', 'Evap', 'RO', 'TotCh', 'SurfCh', 'State', 'NWtrState', 'Drainage', 'SMD', 'FlowCh', 'AddWater', 'ROSoil', 'ROPipe', 'ROImp', 'ROVeg', 'ROWater', 'WUInt', 'WUEveTr', 'WUDecTr', 'WUGrass', 'SMDPaved', 'SMDBldgs', 'SMDEveTr', 'SMDDecTr', 'SMDGrass', 'SMDBSoil', 'StPaved', 'StBldgs', 'StEveTr', 'StDecTr', 'StGrass', 'StBSoil', 'StWater', 'Zenith', 'Azimuth', 'AlbBulk', 'Fcld', 'LAI', 'z0m', 'zdm', 'UStar', 'Lob', 'RA', 'RS', 'Fc', 'FcPhoto', 'FcRespi', 'FcMetab', 'FcTraff', 'FcBuild', 'FcPoint', 'QNSnowFr', 'QNSnow', 'AlbSnow', 'QM', 'QMFreeze', 'QMRain', 'SWE', 'MeltWater', 'MeltWStore', 'SnowCh', 'SnowRPaved', 'SnowRBldgs', 'Ts', 'T2', 'Q2', 'U10', 'RH2']
 
-# final_split_df = # see tmrw
+cover_list = ['LCZ1', 'LCZ2', 'LCZ3', 'LCZ4', 'LCZ5', 'LCZ6', 'LCZ7', 'LCZ8', 'LCZ9', 'LCZ10', 'LCZ11', 'LCZ12', 'LCZ13', 'LCZ14', 'LCZ15', 'LCZ16', 'LCZ17', 'Paved (-)', 'Buildings (-)', 'Grass (-)', 'Deciduous trees (-)', 'Evergreen trees (-)', 'Bare soil (-)', 'Water (-)', 'Mean building height (m)', 'Mean vegetation height (m)', 'Albedo (-)', 'Height-to-width ratio (-)', 'Frontal area index buildings (-)', 'Frontal area index deciduous tree (-)', 'Frontal area index evergeen tree (-)']
 
-final_split_df = pd.MultiIndex(levels=[[],[]],
-                       codes=[[],[]], names=[u'grid', u'timestamp',])
+# Intialize dataframes
+
+final_split_df = pd.MultiIndex(levels=[[],[],[],[]],
+                       codes=[[],[]], names=[u'grid', u'timestamp', u'latitude', u'longitude'])
                        
 final_split_df = pd.DataFrame(index = final_split_df, columns = variable_list)
 final_split_df = final_split_df.rename_axis(columns='var')
 
-# use .concat() and join="inner", so that only the previously selected columns are added.
-final_split_df = pd.concat([final_split_df, test_out_df]) 
+final_split_surf_frac = pd.DataFrame(columns = cover_list)
+final_split_surf_frac.index.name = 'grid'
 print(final_split_df)
-crs_dict = {
-            'proj': 'utm',
-            'zone': int(np.round((183 + site_midpoint_lon) / 6)),
-            'south': site_midpoint_lat < 0,
-        }
 
-crs = CRS.from_dict(crs_dict)
-site_list_df.index[model_run, 
-    for individual_split_site in split_site_list_df.index:
-        individual_split_name = split_site_list_df.iloc[individual_split_site, 0]
-        individual_split_path = f'data/{individual_split_name}/output/grid'
-        split_output_file = individual_split_path / 'df_output_uMF_uLCu.h5'
-
-# convert latlong to UTM for consistency / ease of calculations
-to_utm = Transformer.from_crs(crs_from='EPSG:4326', crs_to=crs)
-split_midpoint_x, split_midpoint_y = to_utm.transform(xx=site_midpoint_lat, yy=site_midpoint_lon)
-
-# identify site boundaries
 split_metre_length = 1000  * split_grid_length
+split_file_count = 0
 
-grid_y_max = split_midpoint_y + (split_metre_length / 2)
-grid_y_min = split_y_max - (split_metre_length)
-grid_x_max = split_midpoint_x + (split_metre_length / 2)
-grid_x_min = split_x_max - (split_metre_length)
+for individual_split_site in split_site_list_df.index:
+    individual_split_name = split_site_list_df.iloc[individual_split_site, 0]
+    individual_split_path = f'data/{individual_split_name}/output/grid'
+    individual_split_df = pd.read_hdf(Path(individual_split_path, 'df_output_uMF_uLCu.h5'))
+    individual_split_lcz = pd.read_csv(Path(individual_split_path, 'df_roi_lcz.csv'), index_col = 'id')
+    individual_split_supyfraction = pd.read_csv(Path(individual_split_path, 'df_roi_suews.csv'), index_col = 'id')
+        
+    individual_split_surf_frac = pd.merge(individual_split_lcz, individual_split_supyfraction, on = 'id')
+        
+    # convert latlong to UTM for consistency / ease of calculations
 
-# +1 to account for the additional sample at the start, [1:] to remove before meshing.
-grid_midpoint_x = np.linspace(grid_x_min, grid_x_max, split_grid_length + 1, endpoint = False)[1:]
-grid_midpoint_y = np.linspace(grid_y_min, grid_y_max, split_grid_length + 1, endpoint = False)[1:]
+    split_site_lat = split_site_list_df.iloc[individual_split_site, 1]
+    split_site_lon = split_site_list_df.iloc[individual_split_site, 2]
+    
+    crs_dict = {
+            'proj': 'utm',
+            'zone': int(np.round((183 + split_site_lat) / 6)),
+            'south': split_site_lon < 0,
+        }
+    crs = CRS.from_dict(crs_dict)
+    to_utm = Transformer.from_crs(crs_from='EPSG:4326', crs_to=crs)
+    
+    split_midpoint_x, split_midpoint_y = to_utm.transform(xx=split_site_lat, yy=split_site_lon)
+    
+    # identify site boundaries
+    grid_y_max = split_midpoint_y + (split_metre_length / 2)
+    grid_y_min = grid_y_max - (split_metre_length)
+    grid_x_max = split_midpoint_x + (split_metre_length / 2)
+    grid_x_min = grid_x_max - (split_metre_length)
 
-# converting back to latlong
-from_utm = Transformer.from_crs(crs_from=crs, crs_to='EPSG:4326')
-grid_midpoint_lat, grid_midpoint_lon = from_utm.transform(xx=grid_midpoint_x, yy=grid_midpoint_y)
+    # +1 to account for the additional sample at the start, [1:] to remove before meshing.
+    grid_midpoint_x = np.linspace(grid_x_min, grid_x_max, split_grid_length + 1, endpoint = False)[1:]
+    grid_midpoint_y = np.linspace(grid_y_min, grid_y_max, split_grid_length + 1, endpoint = False)[1:]
 
-# repeat latlong to form a 1d grid
-split_grid_xx, split_grid_yy = np.meshgrid(grid_midpoint_lat, grid_midpoint_lon)
+    # converting back to latlong
+    from_utm = Transformer.from_crs(crs_from=crs, crs_to='EPSG:4326')
+    grid_midpoint_lat, grid_midpoint_lon = from_utm.transform(xx=grid_midpoint_x, yy=grid_midpoint_y)
 
-split_grid_lat_list = np.ndarray.flatten(split_grid_xx.tolist())
-split_grid_lon_list = np.ndarray.flatten(split_grid_yy.tolist())
+    # repeat latlong to form a 1d grid
+    split_grid_xx, split_grid_yy = np.meshgrid(grid_midpoint_lat, grid_midpoint_lon)
+
+    split_grid_lat = list(np.ndarray.flatten(split_grid_xx))
+    split_grid_lon = list(np.ndarray.flatten(split_grid_yy))
+    
+    # inserting latlong as index
+    split_latlon_iter = int(individual_split_df.shape[0] / len(split_grid_lat))
+    split_grid_lat_iter = np.repeat(split_grid_lat, split_latlon_iter)
+    split_grid_lon_iter = np.repeat(split_grid_lon, split_latlon_iter)
+    individual_split_df.set_index({'latitude': split_grid_lat_iter, 'longitude': split_grid_lon_iter}, append = True, inplace = True)
+    
+    individual_split_surf_frac.insert(0, 'latitude', split_grid_lat)
+    individual_split_surf_frac.insert(1, 'longitude', split_grid_lon)
+        
+    # changing grid numbers
+    file_grid_number = individual_split_df.index.levels[0]
+    modified_grid_numbers = file_grid_number + (split_file_count * split_grid_area)
+    grid_number_dict = dict(list(zip(file_grid_number.to_list(), modified_grid_numbers.to_list())))
+        
+    print(grid_number_dict)
+        
+    individual_split_df.rename(grid_number_dict, level = 'grid', inplace = True)
+    individual_split_surf_frac.rename(index = grid_number_dict, inplace = True)
+        
+    print(f'Processing output file for {individual_split_name}')
+    # merging to final df
+    final_split_surf_frac = pd.concat([final_split_surf_frac, individual_split_surf_frac], join = 'inner')
+    final_split_df = pd.concat([final_split_df, individual_split_df], join = 'inner')
+    split_file_count += 1
+    if split_file_count == 4: # running for the first 4 files only.
+        print(final_split_df.index)
+        print(final_split_df)
+        print(final_split_surf_frac.index)
+        break
 
 
 # final for loop to copy into run_split_models
