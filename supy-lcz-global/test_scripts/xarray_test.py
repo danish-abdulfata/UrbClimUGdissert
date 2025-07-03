81c2f36d2f1428fdb1bfc4d8e2f20e57c67c4206
danish-abdulfata <124814346+danish-abdulfata@users.noreply.github.com>
2025-07-03 12:18:51 +0100

    Update xarray_test.py

--- supy-lcz-global/test_scripts/xarray_test.py
+++ supy-lcz-global/test_scripts/xarray_test.py
@@ -6,17 +6,13 @@ from pyproj import CRS
 from pyproj import Transformer
 import xarray as xr
 
-split_runs_start = time.time()
 split_site_list_df = pd.read_csv(Path('resources/GreaterKL-2017_Y1_M2sp_3sf_R1_splitlist.csv'))
 split_site_list_df.index.name = 'sitename'
-number_of_runs = 3
-split_grid_length = 5
-split_grid_area = split_grid_length**2
-split_run_count = 0
-site_prefix = "GreaterKL-2017_Y1_M2sp_3sf_R1"
 
+site_prefix = "GreaterKL-2017_Y1_M2sp_3sf_R1"
 output_file = './data/consolidated_outputs/'
 variable_list = ['Tsurf', 'QN', 'QF', 'QS', 'QH', 'QE', 'QHlumps', 'QElumps', 'QHresis', 'AlbBulk', 'Fc', 'Ts', 'T2', 'Q2', 'U10', 'RH2']
+
 # hdf5 testing
 
 # final_split_df = pd.read_hdf(Path(output_file, site_prefix + '_consolidated.h5'))
@@ -29,6 +25,7 @@ variable_list = ['Tsurf', 'QN', 'QF', 'QS', 'QH', 'QE', 'QHlumps', 'QElumps', 'Q
 
 # final_split_ds = xr.open_dataset(Path(output_file, site_prefix + '_consolidated.h5'), engine = "h5netcdf", chunks="auto", group = "df", phony_dims="access")
 # print(final_split_ds)
+
 list_of_split_file = []
 
 for individual_split_site in split_site_list_df.index:
@@ -38,7 +35,7 @@ for individual_split_site in split_site_list_df.index:
 
 final_split_ds = xr.open_mfdataset(list_of_split_file, engine = "h5netcdf", data_vars = variable_list)
 
-# might need to use a for loop xr.open then xr.concat instead
+# might need to use a for loop xr.open then xr.concat instead, because coordinate data needs a cleanup and can not be inferred.
 
 print(final_split_ds)
 final_split_ds.to_netcdf(path = Path(output_file, site_prefix + '_consolidatedtest.nc'), mode ='w')
